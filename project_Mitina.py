5225fe3e1b1688a6bf1e3f21d619a3e7264c1798
MitinaN <hedgehog-86@mail.ru>
2020-05-19 10:24:56 +0300

    Create project_Mitina.py

---
+++ project_Mitina.py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+count = 0  # счетчик попыток
+number = np.random.randint(1,101) (1, 101)  # загадали число
+print("Загадано число от 1 до 100")
+
+while True:  # бесконечный цикл
+    predict = int(input())  # предполагаемое число
+    count += 1  # плюсуем попытку
+    if number == predict:
+        break  # выход из цикла, если угадали
+    elif number > predict:
+        print(f"Угадываемое число больше {predict} ")
+    elif number < predict:
+        print(f"Угадываемое число меньше {predict} ")
+
+print(f"Вы угадали число {number} за {count} попыток.")
+
+
+# In[2]:
+
+
+pip install numpy
+
+
+# In[ ]:
+
+
+
+
