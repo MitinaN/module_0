7ca4e24268b53a67202ae025682f61f06170d114
MitinaN <hedgehog-86@mail.ru>
2020-05-19 14:13:31 +0300

    Update project_Mitina.py
    бинарный поиск числа методом деления интервала пополам. Начало поиска в середине

--- project_Mitina.py
+++ project_Mitina.py
@@ -1,30 +1,34 @@
 import numpy as np
 import random
-count = 0  # счетчик попыток
-number = np.random.randint(1,101) (1, 101)  # загадали число
-print("Загадано число от 1 до 100")
-
-while True:  # бесконечный цикл
-    predict = int(input())  # предполагаемое число
-    count += 1  # плюсуем попытку
-    if number == predict:
-        break  # выход из цикла, если угадали
-    elif number > predict:
-        print(f"Угадываемое число больше {predict} ")
-    elif number < predict:
-        print(f"Угадываемое число меньше {predict} ")
-
-print(f"Вы угадали число {number} за {count} попыток.")
 
 
-# In[2]:
+def score_game(game_core):
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
 
 
-pip install numpy
+def game_core_v2(number):
+    """Функция принимает загаданное число и возвращает число попыток. Поиск числа осуществляется бинарным методом"""
+    count = 1
+    predict = 51
+    min_number = 0
+    max_number = 101
+    while number != predict:
+        count += 1
+        if number > predict:
+            min_number = predict  # ограничиваем диапазон угадываемого числа снизу
+            predict += (max_number - predict) // 2
+        elif number < predict:
+            max_number = predict  # ограничиваем диапазон угадываемого числа сверху
+            predict -= (predict - min_number) // 2
+    return count  # выход из цикла, если угадали
 
 
-# In[ ]:
-
-
-
-
+score_game(game_core_v2)
